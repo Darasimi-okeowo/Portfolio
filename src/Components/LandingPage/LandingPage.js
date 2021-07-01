d08b5851c011fbcae871fb2997337fbb47b2b344
Darasimi-okeowo <okeowoadrasimidavid19@gmail.com>
2021-07-01 19:54:07 +0100

    first-commit

---
+++ src/Components/LandingPage/LandingPage.js
@@ -0,0 +1,45 @@
+import React from "react";
+import landingVideo from "./landing-video.mp4";
+// import logo from "../oke-logo/Logo.svg"
+import "./LandingPage.css";
+import Typical from "react-typical";
+
+const LandingPage = () => {
+  return (
+    <div className="header">
+      <video autoPlay loop muted className="video">
+        <source src={landingVideo} type="video/mp4" />
+      </video>
+      <p>logo</p>
+<div className='center'>
+<h1 className="texts">
+        Hello.<mark style={{ color: "#C38D9E", background: "none" }}>I'm</mark>{" "}
+        <mark style={{ color: "#E8A87C", background: "none" }}>Okeowo</mark>{" "}
+        <mark style={{ color: "#EDF5E1", background: "none" }}>
+          Oluwadarasimi David
+        </mark>
+      </h1>
+      <p className="text">
+        <Typical
+          className="loops"
+          loop={Infinity}
+          wrapper="b"
+          steps={[
+            " a software developer",
+            1000,
+            " a student",
+            1000,
+            " a Quick learner",
+            1000,
+            " Someone's boyfriend",
+            1000,
+          ]}
+        />
+      </p>
+</div>
+      
+    </div>
+  );
+};
+
+export default LandingPage;
